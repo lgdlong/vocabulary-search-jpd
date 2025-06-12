8b10ec543ac42f571ec6c189d7d8d2f625810d67
Phung Luu Hoang Long <phungluuhoanglong@gmail.com>
2025-06-12 19:23:28 +0700

    feat: add TypeScript configuration and update dependencies for improved type support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   build: {
